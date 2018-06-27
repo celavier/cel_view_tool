ffb46a1bced6318a5023684f63ede632961949ac
Soren Olsen <soren@celavier.com>
2018-06-27 17:04:14 -0400

    Dev tool initial commit

---
+++ lib/cel_view_tool.rb
@@ -0,0 +1,6 @@
+require "cel_view_tool/version"
+require "cel_view_tool/renderer"
+
+module CelViewTool
+  # Your code goes here...
+end
---
+++ lib/cel_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CelViewTool
+  VERSION = "0.1.0"
+end
